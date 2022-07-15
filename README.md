a48d4626664aaf9bc20a2238d97657701487f524
Mario Reder <mreder1289@gmail.com>
2022-07-15 05:42:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QLytics API
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    if (request.method === 'OPTIONS') {
+      const headers = new Headers();
+      setupCORS(request, headers);
+      return new Response('', { status: 204, headers });
+    }
+    try {
+      const response = new Response('', { status: 204 });
+      setupCORS(request, response.headers);
+      return response;
+    } catch (e) {
+      const headers = new Headers();
+      setupCORS(request, headers);
+      if (e instanceof Error) {
+        console.log('Internal Error', e.message);
+        return new Response(e.message, { status: 500, headers });
+      }
+      console.log('Internal Error', e);
+      return new Response('Unknown Error', { status: 500, headers });
+    }
+  }
+};
+
+function setupCORS(request: Request, headers: Headers) {
+  const origin = request.headers.get('Origin');
+  if (origin != null) {
+    headers.set('Access-Control-Allow-Origin', origin);
+  }
+}
